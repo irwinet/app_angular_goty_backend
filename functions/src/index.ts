387a6a544a035176e3af2af2ddcf5acec6c31d37
irwinet <irwinestradatorres@gmail.com>
2020-12-28 11:35:28 -0500

    Hola Mundo Get

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,17 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+/*
+
+    firebase deploy -> Subir funciones GET POST a Produccion
+    firebase serve -> Probar localmente
+    
+*/
+
+export const helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.json({
+    mensaje: "Hola mundo desde funciones"
+  });
+});
