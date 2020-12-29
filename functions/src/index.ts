01a6d81cb168f3768a7893fcc2a14e50a69ac177
irwinet <irwinestradatorres@gmail.com>
2020-12-29 18:00:37 -0500

    Add Express And Cors

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as cors from 'cors';
 
 const serviceAccount = require('./serviceAccountKey.json');
 
@@ -41,3 +43,20 @@ export const getGOTY = functions.https.onRequest( async (request, response) => {
   response.json(juegos);
 
 });
+
+
+//Servidor Express -> Crear Servicios en Node
+const app = express();
+
+app.use(cors({origin:true}));
+
+app.get('/goty', async (req, res)=>{
+  const gotyRef = db.collection('goty');
+  const docsSnap = await gotyRef.get();
+  const juegos = docsSnap.docs.map(doc=>doc.data());
+
+  res.json(juegos);
+});
+
+
+exports.api = functions.https.onRequest(app);
